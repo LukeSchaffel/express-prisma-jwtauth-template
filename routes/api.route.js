5997cc783636a17aa4ef7fc0a06422e647d1695f
LukeSchaffel <lukeschaffel@gmail.com>
2022-09-26 10:23:29 -0400

    basic crud

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,7 +1,80 @@
 const router = require('express').Router();
+const { PrismaClient } = require('@prisma/client')
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+const prisma = new PrismaClient()
+
+//get all users
+router.get('/users', async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany({})
+    res.json(users)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 });
+
+//get one user by ID
+router.get('/users/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+
+});
+
+//create user
+router.post('/users/signup', async (req, res, next) => {
+  try {
+    const user = await prisma.user.create({
+      data: req.body
+    })
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+});
+
+
+//update user
+router.patch('/users/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user = await prisma.user.update({
+      where: {
+        id: id
+      },
+      data: req.body
+    })
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+});
+
+router.delete('/users/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: id
+      }
+    })
+    res.json(deletedUser)
+  } catch (error) {
+    next(error)
+  }
+});
+
 
 module.exports = router;
